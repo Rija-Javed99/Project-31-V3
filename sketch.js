dc478149f07ded38bd23533a651f64ad682edc85
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-06-09 18:11:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,30 +19,10 @@ function setup() {
      divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
    }
 
-
-    for (var j = 75; j <=width; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,75));
-    }
-
-    for (var j = 50; j <=width-10; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,175));
-    }
-
-     for (var j = 75; j <=width; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,275));
-    }
-
-     for (var j = 50; j <=width-10; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,375));
-    }
+//add for loops for each plinko
+  
+  
+  
 
     
 
